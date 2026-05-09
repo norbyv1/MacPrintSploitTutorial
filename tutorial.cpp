e9bf31d1b634531e51db1395445ced8f6aa64faa
norbyv1 <127006485+norbyv1@users.noreply.github.com>
2026-05-08 19:32:55 -0700

    New

--- tutorial.cpp
+++ tutorial.cpp
@@ -1,28 +1,22 @@
 #include <thread>
 #include <mach-o/dyld.h>
 
-int main()
-{
-    std::this_thread::sleep_for(std::chrono::seconds(13)); // give ample time to get into a game (you can modify this value based on your Mac's performance)
-    uintptr_t offset = 0x1010adc10;
-    typedef int (*printdef)(int, const char*, ...);
-    auto printfunction = reinterpret_cast<printdef>(_dyld_get_image_vmaddr_slide(0) + (uintptr_t)offset);
-
-    auto delayprint = [&](int type, const char* message, std::chrono::milliseconds delay) {
-        std::this_thread::sleep_for(delay);
-        printfunction(type, message);
-    };
-
-    delayprint(0, "print", std::chrono::milliseconds(200)); // add delay(s)
-    delayprint(1, "info", std::chrono::milliseconds(200));  
-    delayprint(2, "warn", std::chrono::milliseconds(200));  
-    delayprint(3, "error", std::chrono::milliseconds(200));
-
-    return 0;
-}
-
 __attribute__((constructor)) void entry()
 {
-    printf("[ Custom printsploit dylib injected ]\n");
-    std::thread(main).detach();
+    printf("Custom printsploit loaded\n");
+    std::thread([] {
+        std::this_thread::sleep_for(std::chrono::seconds(13)); // give ample time to get into a game (you can modify this value based on your Mac's performance)
+        uintptr_t offset = 0x1010adc10;
+        void (*printfunction)(int, const char*, ...) = reinterpret_cast<void(*)(int, const char*, ...)>(_dyld_get_image_vmaddr_slide(0) + offset);
+    
+        auto print = [&](int type, const char* message) {
+            std::chrono::milliseconds(200)
+            printfunction(type, message);
+        };
+    
+        delayprint(0, "print");
+        delayprint(1, "info");
+        delayprint(2, "warn");
+        delayprint(3, "error");
+    }).detach();
 }
